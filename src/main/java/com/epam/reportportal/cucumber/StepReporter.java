bc45bb1648ea23bca79f14c13fa1bda818b22b5d
yumfriez <budaevqwerty@gmail.com>
2019-07-23 14:33:21 +0300

    Code ref retrieving via Reflection API impl

--- src/main/java/com/epam/reportportal/cucumber/StepReporter.java
+++ src/main/java/com/epam/reportportal/cucumber/StepReporter.java
@@ -24,9 +24,14 @@ import com.epam.reportportal.listeners.Statuses;
 import com.epam.ta.reportportal.ws.model.StartTestItemRQ;
 import cucumber.api.Result;
 import cucumber.api.TestStep;
+import cucumber.runtime.StepDefinitionMatch;
 import gherkin.ast.Step;
 import io.reactivex.Maybe;
+import io.reactivex.annotations.Nullable;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Calendar;
 
 /**
@@ -49,8 +54,6 @@ import java.util.Calendar;
  * @author Serhii Zharskyi
  */
 public class StepReporter extends AbstractReporter {
-
-	public static final String METHOD_OPENING_BRACKET= "(";
 
 	protected Maybe<String> currentStepId;
 	protected Maybe<String> hookStepId;
@@ -76,8 +79,7 @@ public class StepReporter extends AbstractReporter {
 		rq.setDescription(Utils.buildMultilineArgument(testStep));
 		rq.setStartTime(Calendar.getInstance().getTime());
 		rq.setType("STEP");
-		String codeLocation = testStep.getCodeLocation();
-		rq.setLocation(codeLocation.substring(0, codeLocation.indexOf(METHOD_OPENING_BRACKET)));
+		rq.setCodeRef(Utils.getCodeRef(testStep));
 		currentStepId = RP.get().startTestItem(currentScenarioContext.getId(), rq);
 	}
 
