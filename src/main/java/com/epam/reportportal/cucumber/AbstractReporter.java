df25f7b5d860d2462e9b6725fc5232655886151b
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2024-04-04 15:48:36 +0300

    Dependencies update

--- src/main/java/com/epam/reportportal/cucumber/AbstractReporter.java
+++ src/main/java/com/epam/reportportal/cucumber/AbstractReporter.java
@@ -27,6 +27,7 @@ import com.epam.reportportal.service.item.TestCaseIdEntry;
 import com.epam.reportportal.service.tree.TestItemTree;
 import com.epam.reportportal.utils.*;
 import com.epam.reportportal.utils.files.ByteSource;
+import com.epam.reportportal.utils.http.ContentType;
 import com.epam.reportportal.utils.markdown.MarkdownUtils;
 import com.epam.reportportal.utils.properties.SystemAttributesExtractor;
 import com.epam.ta.reportportal.ws.model.FinishExecutionRQ;
@@ -46,7 +47,6 @@ import gherkin.ast.Step;
 import gherkin.ast.Tag;
 import gherkin.pickles.*;
 import io.reactivex.Maybe;
-import okhttp3.MediaType;
 import org.apache.commons.lang3.tuple.Pair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -535,15 +535,7 @@ public abstract class AbstractReporter implements Formatter {
 	 * @param data     data to attach
 	 */
 	protected void embedding(String mimeType, byte[] data) {
-		String type = ofNullable(mimeType).filter(m -> {
-			try {
-				MediaType.get(m);
-				return true;
-			} catch (IllegalArgumentException e) {
-				LOGGER.warn("Incorrect media type '{}'", m);
-				return false;
-			}
-		}).orElseGet(() -> getDataType(data));
+		String type = ofNullable(mimeType).filter(ContentType::isValidType).orElseGet(() -> getDataType(data));
 		String attachmentName = ofNullable(type).map(t -> t.substring(0, t.indexOf("/"))).orElse("");
 		ReportPortal.emitLog(new ReportPortalMessage(ByteSource.wrap(data), type, attachmentName),
 				"UNKNOWN",
